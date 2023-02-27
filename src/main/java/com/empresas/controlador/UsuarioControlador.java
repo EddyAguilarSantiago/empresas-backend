67e13e0546823c13360b758e8ac6da3552fb1eac
EddyAguilarSantiago <jesuseduardoaguilars@gmail.com>
2023-02-27 17:16:06 -0600

    Se agrega metodo para guardar usuario

--- src/main/java/com/empresas/controlador/UsuarioControlador.java
+++ src/main/java/com/empresas/controlador/UsuarioControlador.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +15,21 @@ import com.empresas.repositorio.UsuarioRepositorio;
 
 @RestController
 @RequestMapping("")
-@CrossOrigin(origins = "https://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioControlador {
 	
 	@Autowired
 	private UsuarioRepositorio repositorio;
 	
-
 	//Método para listar las empresas
 	@GetMapping("/usuarios")
 	public List<Usuario> listarTodasLasEmpresas(){
 		return repositorio.findAll();
+	}
+	
+	//Método para guardar usuario
+	@PostMapping("/usuarios")
+	public Usuario guardarUsuario(@RequestBody Usuario usuario) {
+		return repositorio.save(usuario);
 	}
 }
