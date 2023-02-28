c2f237aff95f0158de650a90c164da2e66cb2110
EddyAguilarSantiago <jesuseduardoaguilars@gmail.com>
2023-02-28 14:31:38 -0600

    Se agrega metodo para buscar usuario por su ID

--- src/main/java/com/empresas/controlador/UsuarioControlador.java
+++ src/main/java/com/empresas/controlador/UsuarioControlador.java
@@ -3,13 +3,16 @@ package com.empresas.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.empresas.excepciones.ResourceNotFoundException;
 import com.empresas.modelo.Usuario;
 import com.empresas.repositorio.UsuarioRepositorio;
 
@@ -31,5 +34,13 @@ public class UsuarioControlador {
 	@PostMapping("/usuarios")
 	public Usuario guardarUsuario(@RequestBody Usuario usuario) {
 		return repositorio.save(usuario);
+	}
+	
+	//Método para buscar un usuario por su ID
+	@GetMapping("/usuarios/{id}")
+	public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable Long id){
+		Usuario usuario = repositorio.findById(id)
+						  .orElseThrow(() -> new ResourceNotFoundException("No existe el usuario con ID: + id")); //Si no existe se manda este mensaje 
+		return ResponseEntity.ok(usuario);
 	}
 }
